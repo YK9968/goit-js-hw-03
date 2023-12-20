f497dd6c2eb1b50f04b15fa096978d6a41bfb1c0
YK9968 <robka4an@gmail.com>
2023-12-20 13:32:30 +0200

    finish HW

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function makeArray(firstArray, secondArray, maxLength) {
+  const newArray = firstArray.concat(secondArray);
+  const lengthArray = [];
+  for (let indexArray = 0; indexArray + 1 <= maxLength; indexArray++) {
+    lengthArray.push(newArray[indexArray]);
+  }
+
+  return lengthArray;
+}
+
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
